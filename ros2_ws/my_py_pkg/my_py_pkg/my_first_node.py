b8ae12f37bfedb1f18259e75e21082cf35e66e3a
khaino <pskhai@gmail.com>
2024-06-12 16:38:23 -0700

    template node

--- ros2_ws/my_py_pkg/my_py_pkg/my_first_node.py
+++ ros2_ws/my_py_pkg/my_py_pkg/my_first_node.py
@@ -6,7 +6,13 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.counter = 0
         self.get_logger().info("Hello ROS2!")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter += 1
+        self.get_logger().info("Hello " + str(self.counter))
 
 def main(args=None):
     rclpy.init(args=args)
