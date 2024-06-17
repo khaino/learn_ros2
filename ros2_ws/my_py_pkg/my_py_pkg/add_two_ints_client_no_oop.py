c51215a9120394a421c5c8b3aefb955395b5f609
khaino <pskhai@gmail.com>
2024-06-17 16:29:47 -0700

    add a simple client without oop

---
+++ ros2_ws/my_py_pkg/my_py_pkg/add_two_ints_client_no_oop.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from example_interfaces.srv import AddTwoInts
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Node("add_two_ints_no_oop")
+
+    client = node.create_client(AddTwoInts, "add_two_ints")
+    while not client.wait_for_service(1):
+        node.get_logger().warn("Waiting for server add_two_ints")
+
+    request = AddTwoInts.Request()
+    request.a = 7
+    request.b = 21
+
+    future = client.call_async(request)
+    rclpy.spin_until_future_complete(node, future)
+    try:   
+        response = future.result()
+        node.get_logger().info(str(request.a) + " + " + str(request.b) + " = " + str(response.sum))
+    except Exception as e:
+        node.get_logger().error("Service call failed: %r", (e,))
+    rclpy.shutdown()
+
+if __name__ == "__mian__":
+    main()
