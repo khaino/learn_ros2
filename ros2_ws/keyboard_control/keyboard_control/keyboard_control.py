56feb423f48e8faf83acbf451aedd4abe0547f25
YeachanOkh <yeachanokh@ubuntu22.04.myguest.virtualbox.org>
2024-07-10 18:29:02 -0700

    keyboard with publisher

--- ros2_ws/keyboard_control/keyboard_control/keyboard_control.py
+++ ros2_ws/keyboard_control/keyboard_control/keyboard_control.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python3
-
 import rclpy
 import time
+import geometry_msgs.msg
 from rclpy.node import Node
 from pynput import keyboard
 
 msg = """
 Direction Control
 -----------
-     i    
-j         l
-     ,
+     w    
+a         d
+     s
 -----------
-i : forward
-, : reverse
-j : left steering
-l : right steering
+w : forward
+s : reverse
+a : left steering
+d : right steering
 -----------
 Speed Control
 -----------
@@ -33,69 +32,65 @@ class KeyboardControl(Node):
         self.logger = self.get_logger()
         self.logger.info("keyboard_control initialized!")
 
-        self.speed = 1
+        self.speed = 0.5
         self.direction_control = {
-            'i': 0,
-            'j': 0,
-            'l': 0,
-            ',': 0
-        }
-        self.speed_control = {
-            
+            'w': 0,
+            'a': 0,
+            'd': 0,
+            's': 0
         }
 
-    def _log(self, msg):
-        self.get_logger().info(msg)
-
-    def _set_speed(self, speed):
-        self.speed = min(KeyboardControl.MAX_SPEED, speed)
+        # Publisher for Twist messages
+        self.publisher = self.create_publisher(geometry_msgs.msg.Twist, 'keyboard_control', 10)
 
     def start(self):
-        self.get_logger().info(f"____start_____")
-        running = True
-        while running:
-            with keyboard.Listener(
-                on_press=self.on_press,
-                on_release=self.on_release) as listener:
-                listener.join()
+        self.logger.info("Starting keyboard listener...")
+        with keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release) as listener:
+            listener.join()
 
-                
     def on_press(self, key):
         try:
             if hasattr(key, 'char') and key.char.lower() in self.direction_control:
                 k = key.char.lower()
-                print(f"Key pressed: {key.char}")
                 self.direction_control[k] = 1
 
-                if self.direction_control['i'] == 1 and self.direction_control[','] == 1:
-                    self.direction_control['i'] = 0
-                    self.direction_control[','] = 0
-                    self.logger.warn("Do not press `i' and `,` at the same time!!")
+                if self.direction_control['w'] == 1 and self.direction_control['s'] == 1:
+                    self.direction_control['w'] = 0
+                    self.direction_control['s'] = 0
+                    self.logger.warn("Do not press 'w' and `s` at the same time!!")
                     self.logger.info(msg)
                     
-                elif self.direction_control['j'] == 1 and self.direction_control['l'] == 1:
-                    self.direction_control['j'] = 0
-                    self.direction_control['l'] = 0
-                    self.logger.warn("Do not press `j' and `l` at the same time!!")
+                elif self.direction_control['a'] == 1 and self.direction_control['d'] == 1:
+                    self.direction_control['a'] = 0
+                    self.direction_control['d'] = 0
+                    self.logger.warn("Do not press `a' and `d` at the same time!!")
                     self.logger.info(msg)
 
-                print(self.direction_control)
+                self.publish_twist()
+                self.logger.info(f"Direction control: {self.direction_control}\n")
             else:
-                print(f"Invalid key pressed: {key}")
+                self.logger.info(f"Invalid key pressed: {key}")
         except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
+            self.logger.info('Special key {0} pressed'.format(key))
             
     def on_release(self, key):
         if hasattr(key, 'char') and key.char.lower() in self.direction_control:
             k = key.char.lower()
             self.direction_control[k] = 0
-            print(self.direction_control)
-            print('{0} released'.format(key))
+            self.publish_twist()
+            self.logger.info(f"Direction control: {self.direction_control}\n")
         if key == keyboard.Key.esc:
-            # Stop listener
             return False
 
+    def publish_twist(self):
+        msg = geometry_msgs.msg.Twist()
+        # Set linear and angular velocities based on direction control and speed
+        msg.linear.x = self.speed * (self.direction_control['w'] - self.direction_control['s'])
+        msg.angular.z = self.speed * (self.direction_control['d'] - self.direction_control['a'])
+        self.publisher.publish(msg)
+        self.logger.info(f"Twist message published:\n")
 
 def main(args=None):
     rclpy.init(args=args)
