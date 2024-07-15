ff6f9cd57faef38eb598e420e2b52d82611d3632
khaino <pskhai@gmail.com>
2024-07-15 13:57:34 -0700

    add speed control

--- ros2_ws/keyboard_control/keyboard_control/keyboard_control.py
+++ ros2_ws/keyboard_control/keyboard_control/keyboard_control.py
@@ -10,23 +10,32 @@ Direction Control
 -----------
      i    
 j         l
-     ,
+     k
 -----------
 i : forward
-, : reverse
+k : reverse
 j : left steering
 l : right steering
 -----------
 Speed Control
 -----------
-e : speed up (+1)
-d : speed down (-1)
+w : speed up (+1)
+s : speed down (-1)
 -----------
 """
 
 class KeyboardControl(Node):
 
-    MAX_SPEED = 10
+    MIN_SPEED = 1
+    MAX_SPEED = 2
+    SPEED_STEP = 0.1
+    SPEED_UP = 'w'
+    SPEED_DOWN = 's'
+
+    FORWARD = 'i'
+    REVERSE = 'k'
+    LEFT = 'j'
+    RIGHT = 'l'
 
     def __init__(self):
         super().__init__("keyboard_control")
@@ -35,17 +44,12 @@ class KeyboardControl(Node):
 
         self.speed = 1
         self.direction_control = {
-            'i': 0,
-            'j': 0,
-            'l': 0,
-            ',': 0
+            self.FORWARD: 0,
+            self.REVERSE: 0,
+            self.LEFT: 0,
+            self.RIGHT: 0
         }
-        self.speed_control = {
-            
-        }
-
-    def _log(self, msg):
-        self.get_logger().info(msg)
+        self.speed_control = {self.SPEED_UP, self.SPEED_DOWN}
 
     def _set_speed(self, speed):
         self.speed = min(KeyboardControl.MAX_SPEED, speed)
@@ -59,43 +63,59 @@ class KeyboardControl(Node):
                 on_release=self.on_release) as listener:
                 listener.join()
 
-                
     def on_press(self, key):
         try:
-            if hasattr(key, 'char') and key.char.lower() in self.direction_control:
-                k = key.char.lower()
-                print(f"Key pressed: {key.char}")
-                self.direction_control[k] = 1
+            if not hasattr(key, 'char'):
+                self.logger.warn(f"Invalid key pressed: {key}")
+                self.logger.info(msg)
+                return
+            
+            ch = key.char.lower()
+            self.logger.info(f"Key pressed: {ch}")
 
-                if self.direction_control['i'] == 1 and self.direction_control[','] == 1:
-                    self.direction_control['i'] = 0
-                    self.direction_control[','] = 0
-                    self.logger.warn("Do not press `i' and `,` at the same time!!")
+            if ch in self.direction_control:
+                self.direction_control[ch] = 1
+                if self.direction_control[self.FORWARD] == 1 and self.direction_control[self.REVERSE] == 1:
+                    self.direction_control[self.FORWARD] = 0
+                    self.direction_control[self.REVERSE] = 0
+                    self.logger.warn(f"Do not press {self.FORWARD} and {self.REVERSE} at the same time!!")
                     self.logger.info(msg)
                     
-                elif self.direction_control['j'] == 1 and self.direction_control['l'] == 1:
-                    self.direction_control['j'] = 0
-                    self.direction_control['l'] = 0
-                    self.logger.warn("Do not press `j' and `l` at the same time!!")
+                elif self.direction_control[self.LEFT] == 1 and self.direction_control[self.RIGHT] == 1:
+                    self.direction_control[self.LEFT] = 0
+                    self.direction_control[self.RIGHT] = 0
+                    self.logger.warn(f"Do not press {self.LEFT} and {self.RIGHT} at the same time!!")
                     self.logger.info(msg)
+            elif ch in self.speed_control:
+                self.update_speed(ch)
+            self.display_current_state()
 
-                print(self.direction_control)
-            else:
-                print(f"Invalid key pressed: {key}")
         except AttributeError:
-            print('special key {0} pressed'.format(
+            self.logger.warn('Special key {0} pressed'.format(
                 key))
             
     def on_release(self, key):
         if hasattr(key, 'char') and key.char.lower() in self.direction_control:
             k = key.char.lower()
             self.direction_control[k] = 0
-            print(self.direction_control)
-            print('{0} released'.format(key))
+            self.logger.info('{0} released'.format(key))
         if key == keyboard.Key.esc:
-            # Stop listener
             return False
+        
+    def update_speed(self, key):
+        if key == self.SPEED_UP:
+            self.speed = min(self.speed + self.SPEED_STEP, self.MAX_SPEED)
+        elif key == self.SPEED_DOWN:
+            self.speed = max(self.speed - self.SPEED_STEP, self.MIN_SPEED)
 
+    def display_current_state(self):
+        forward = self.direction_control[self.FORWARD]
+        reverse = self.direction_control[self.REVERSE]
+        left = self.direction_control[self.LEFT]
+        right = self.direction_control[self.RIGHT]
+        speed = round(self.speed, 1)
+        msg = f"speed: {speed}, forward: {forward}, reverse: {reverse}, left: {left}, right: {right}"
+        self.logger.info(msg)
 
 def main(args=None):
     rclpy.init(args=args)
